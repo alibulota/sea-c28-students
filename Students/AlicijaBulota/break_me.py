3741f5d3e05ceb544508a9c3d8f2c58270e3eb24
Alicija Bulota <aabulota@OldFaithful.local>
2014-12-03 20:24:27 -0800

    created TypeError and moved to student folder

---
+++ Students/AlicijaBulota/break_me.py
@@ -0,0 +1,29 @@
+#TypeError
+
+print("cat" + 100)
+
+#SyntaxError
+def dog(object):
+    if dog is cat;
+        print("DOG!")
+
+#Cris' ex:
+#def bad_func():
+#   eval("foo = 'This is bad")
+
+#bad_func()
+
+
+#NameError
+def animal():
+    if cats is kitten:
+        print('CATS!')
+
+cats(kitten)
+
+#AttributeError
+class animal(object):
+    def reptile(self):
+        self.lizard
+
+animal().reptile()
