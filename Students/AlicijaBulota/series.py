d11c5c6d9d12810c02a036cec20db17175cb0118
Alicija Bulota <aabulota@smcm.edu>
2014-12-08 19:37:04 -0800

    changes to lucas function and formatting

--- Students/AlicijaBulota/series.py
+++ Students/AlicijaBulota/series.py
@@ -1,9 +1,8 @@
-#Fibonacci Series
+# Fibonacci Series
 
 
 def fibonacci(n):
-    """Fibonacci series function to return
-    nth value in series using recursion"""
+    """Fibonacci function to return nth value in series using recursion."""
     if not isinstance(n, int):
         raise ValueError
     if n < 0:
@@ -15,31 +14,27 @@ def fibonacci(n):
     else:
         return fibonacci(n - 1) + fibonacci(n - 2)
 
-#
-#Lucas Numbers
+# Lucas Numbers
 
 
 def lucas(n):
-    """function to find nth value for
-    Lucas Numbers"""
+    """function to find nth value for Lucas Numbers."""
     if not isinstance(n, int):
         raise ValueError
     if n < 0:
         return None
-    if n == 2:
+    if n == 0:
         return 2
     elif n == 1:
         return 1
     else:
         return lucas(n - 1) + lucas(n - 2)
 
-#
-#sum_series
+# sum_series
 
 
 def sum_series(n, opt_par0=0, opt_par1=1):
-    """function to find nth value in summed
-    integer sequence"""
+    """function to find nth value in summed integer sequence."""
     if not isinstance(n, int):
         raise ValueError
     if n < 0:
@@ -71,7 +66,7 @@ if __name__ == '__main__':
         (5, 11)
     ]
 
-#test to make sure invalid inputs are raising errors
+# test to make sure invalid inputs are raising errors
 
     for invalid_val in (8.3, 'z', []):
         try:
@@ -85,8 +80,8 @@ if __name__ == '__main__':
             print"Invalid input did not raise error."
             assert False
 
-#series of assertions to test for expected output values for
-#each input value per function
+# series of assertions to test for expected output values for
+# each input value per function
 
     for input_, output in fib_vals:
         assert fibonacci(input_) == output
