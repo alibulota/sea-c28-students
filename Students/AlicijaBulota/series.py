dbb4e4e9e5e3d15bfd250738ab33b42245ff2af0
Alicija Bulota <aabulota@smcm.edu>
2014-12-08 16:57:52 -0800

    added function to find nth value for determined lucas or fibonacci function

--- Students/AlicijaBulota/series.py
+++ Students/AlicijaBulota/series.py
@@ -4,6 +4,10 @@
 def fibonacci(n):
     """Fibonacci series function to return
     nth value in series using recursion"""
+    if not isinstance(n, int):
+        raise ValueError
+    if n < 0:
+        return None
     if n == 0:
         return 0
     elif n == 1:
@@ -11,15 +15,42 @@ def fibonacci(n):
     else:
         return fibonacci(n - 1) + fibonacci(n - 2)
 
+#
 #Lucas Numbers
 
 
 def lucas(n):
     """function to find nth value for
     Lucas Numbers"""
+    if not isinstance(n, int):
+        raise ValueError
+    if n < 0:
+        return None
     if n == 2:
         return 2
     elif n == 1:
         return 1
     else:
         return lucas(n - 1) + lucas(n - 2)
+
+#
+#sum_series
+
+
+def sum_series(n, opt_par0=0, opt_par1=1):
+    """function to find nth value in summed
+    integer sequence"""
+    if not isinstance(n, int):
+        raise ValueError
+    if n < 0:
+        return None
+    if n == 0:
+        return opt_par0
+    elif n == 1:
+        return opt_par1
+    else:
+        return sum_series(n - 1, opt_par0, opt_par1) +
+        sum_series(n - 2, opt_par0, opt_par1)
+
+if __name__ == '__main__':
+    fib_vals = []
