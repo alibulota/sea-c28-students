d9d67f03f1740b996891da91c15c0c9b1c3a7633
Alicija Bulota <aabulota@smcm.edu>
2014-12-10 19:58:37 -0800

    function to add fruit to beginning of list by + and ainsert, display all fruit beginning with P

--- Students/AlicijaBulota/list_lab.py
+++ Students/AlicijaBulota/list_lab.py
@@ -20,3 +20,26 @@ ind = int(raw_input("Provide a number for a corresponding fruit: "))
 print "The number %i corresponds to %s" % (ind, fruit[ind - 1])
 
 print "The basket contains", fruit, "."
+
+# add fruit to beginning of list (+), print.
+fruit = ["Strawberrys"] + fruit
+
+print "New fruit at top of list:", fruit
+
+# add fruit to beginning of list (insert), print.
+fruit.insert(0, "Blueberrys")
+
+print "New fruit at top of list:", fruit
+
+# display all fruits that begin with "P", for loop
+print "All fruits that start with 'P' are listed below."
+for first_letter in fruit:
+    if first_letter[0] == 'P':
+        print first_letter
+
+# remove last fruit from list
+print "The fruit basket now contains:", fruit
+
+del fruit[len(fruit) - 1]
+
+print "Last item removed from list:", fruit
