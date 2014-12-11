88b8d3a8c9c802de17e3f05d202e634964c29cc6
Alicija Bulota <aabulota@smcm.edu>
2014-12-10 16:09:41 -0800

    added tests, test pass, words encrypted

--- Students/AlicijaBulota/rot13.py
+++ Students/AlicijaBulota/rot13.py
@@ -1,7 +1,13 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
+# source:https://docs.python.org/2/library/codecs.html#standard-encodings
 
 
 def rot13(text):
     """rot13 encoding:Returns the Caesar-cypher encryption of the operand"""
     return text.encode('rot13')
+
+if __name__ == '__main__':
+    code = "Python is AWESOME"
+
+    print rot13(code)
