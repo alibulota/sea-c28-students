c5f00209004d194d120b4ee243c93a1d76a4685b
Alicija Bulota <aabulota@smcm.edu>
2014-12-28 20:20:08 -0800

    added function to remove leading and trailing white spaces using comprehension

--- Students/AlicijaBulota/fun_files.py
+++ Students/AlicijaBulota/fun_files.py
@@ -21,3 +21,27 @@ def funct(file_name):
     for line in rem_lines:
         write_file.write(line + '\n')
     print '"{}" has no leading or trailing white spaces.'.format(file_name)
+
+
+def comp_funct(file_name):
+    """ using comprehension remove leading and trailing white spaces"""
+    file_read = codecs.open(file_name, 'r')
+    file_lines = file_read.readlines()
+    print file_lines
+    rem_lines = [line.strip() for line in file_lines]
+    print rem_lines
+    file_read.close()
+
+    while True:
+        write_out = raw_input("New file (c) or overwrite existing file (o): ")
+        if write_out.lower() == 'o':
+            break
+        elif write_out.lower() == 'c':
+            file_name = raw_input("New file name: ")
+            break
+
+    write_file = codecs.open(file_name, 'w')
+
+    for line in rem_lines:
+        write_file.write(line + '\n')
+    print '"{}" has no leading or trailing white spaces.'.format(file_name)
