da1568804e55013c35f90ffaada4538e9ccdd6ae
Alicija Bulota <aabulota@smcm.edu>
2014-12-30 20:23:44 -0800

    added generators for doubler, fib seq, prime numbers

--- Students/AlicijaBulota/generator.py
+++ Students/AlicijaBulota/generator.py
@@ -9,3 +9,30 @@ def sum_int():
         yield i
         n += 1
         i = i + n
+
+
+def doubler():
+    i = 1
+    while True:
+        yield i
+        i = i * 2
+
+
+def fib_seq():
+    i = 1
+    last = 0
+    while True:
+        yield i
+        i = i + last
+        last = i - last
+
+
+def prime():
+    i = 1
+    inum = [1]
+    while True:
+        while inum != []:
+            i += 1
+            inum = [1 for x in range(2, i) if not i % x]
+        yield i
+        inum = [1]
