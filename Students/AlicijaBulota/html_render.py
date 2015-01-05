02dcfdf26332ed4da73568df31bcd418b45af86b
Alicija Bulota <aabulota@smcm.edu>
2015-01-04 17:21:25 -0800

    completed step 1

---
+++ Students/AlicijaBulota/html_render.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python
+
+"""
+Python class example.
+"""
+
+# The start of it all:
+# Fill it all in here.
+
+
+class Element(object):
+    tag = u"html"
+    indent = u"    "
+
+    def __init__(self, content=None, **kwargs):
+        if content is not None:
+            self.content = [content]
+        else:
+            self.content = []
+        self.attr = kwargs
+
+    def append(self, text):
+        """Add text string to content."""
+        self.content.append(text)
+
+    def render(self, file_out, ind=u""):
+        """Render tag and string in conent.
+        Keyword arguments:
+        file_out -- File name in which to output render
+        ind -- amount to indent tag (default "")
+        """
+        attr_str = u"".join([u' {}="{}"'.format(k, v) for k, v in
+                            self.attr.items()])
+        file_out.write(u'{}<{}{}>\n'.format(ind, self.tag, attr_str))
+        for item in self.content:
+            try:
+                item.render(file_out, self.indent + ind)
+            except AttributeError:
+                file_out.write(self.indent + ind + item + '\n')
+        file_out.write(u'{}</{}>\n'.format(ind, self.tag))
