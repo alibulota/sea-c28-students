4a19659e837eb30e4115bd14bb10c98d3fa31f39
Alicija Bulota <aabulota@smcm.edu>
2015-01-04 21:56:01 -0800

    added functional code to pseudo code for creating a report of donors, donations, etc.

--- Students/AlicijaBulota/mailroom.py
+++ Students/AlicijaBulota/mailroom.py
@@ -7,19 +7,19 @@ import os
 def thanks(donor_dict):
     """full name. list of names, quit, or add name"""
     while True:
-        name = u'' + safe_input("Input name of donor for letter, \
+        donor_name = u'' + safe_input("Input name of donor for letter, \
             'list' for listing of all donors or 'quit' to exit")
         if donor_name.lower() == u'list':
-            for donor_name in donor_dict.keys():
-                print donor_name
-        elif name == u'quit':
+            for donor in donor_dict.keys():
+                print donor
+        elif donor_name == u'quit':
             return donor_dict
         else:
-            donor_dict.setdefault(name, [])
+            donor_dict.setdefault(donor_name, [])
             break
 
     amount = u'' + safe_input("Enter donation amount for \
-        %s. Type 'quit' to exit " % name)
+        %s. Type 'quit' to exit " % donor_name)
     if amount == u'quit':
         return donor_dict
     while True:
@@ -33,35 +33,44 @@ def thanks(donor_dict):
     else:
         break
 
-    donor_dict[name].append(amount)
+    donor_dict[donor_name].append(amount)
     try:
         os.mkdir('./mail')
         print './folder created'
     except OSError:
         print './folder exists'
 
-    letter_folder = "./mail/{}_ty.txt".format(name)
+    letter_folder = "./mail/{}_ty.txt".format(donor_name)
     letter = codecs.open(letter_folder, 'w')
     letter.write("Dear {}, \n\n\
         \t Thank you for your generous donation and support \
         to our organization. \n\n\
-        Sincerely, \n Alicija Bulota".format(name, amount))
+        Sincerely, \n Alicija Bulota".format(donor_name, amount))
     letter.close()
     print "letter saved in " + letter_folder
     return donor_dict
 
 
-def report(rep_list):
+def report(donations):
     """create report of donations"""
     donation_rep = []
-    n_long = 10  #extra space for longer names in forma
+    spacing = 10
     for donor in donations.keys():
-        if len(donor) > n_long:
-            n_long = len(donor)
+        if len(donor) > spacing:
+            spacing = len(donor)
+        total = sum(donations[donor])
+        count = len(donations[donor])
         if count == 0:
             avg = 0
         else:
-            avg = total/count
-        #sort list by total amount starting with greatest
-    print (# the list of donors with ordered amounts)
+            avg = total / count
+        donation_rep.append([donor, total, count, avg])
+    donation_rep.sort(key=second, reverse=True)
 
+    print "{name:<{n}} {total:>10} {count:>10} {average:>10}".\
+        format(n=spacing, name="Name", total="Total",
+               count="Count", average="Average")
+    for donor in donation_rep:
+        print "{name:<{n}} {total:>10} {count:>10} {average:>10}".\
+            format(n=spacing, name=donor[0], total=donor[1], count=donor[2],
+                   average=donor[3])
